7ae5c97ac3a6b49ca7e3f2b17827dc52e0ff1061
ratichi <ratigugutishvili2004@gmail.com>
2025-07-01 04:23:00 +0400

    finale

--- kapo-back/data-source.ts
+++ kapo-back/data-source.ts
@@ -1,13 +1,15 @@
 import { DataSource } from 'typeorm';
 import { Product } from './src/product/entities/product.entity'; // adjust as needed
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 export const AppDataSource = new DataSource({
   type: 'mysql',
-  host: 'localhost',
-  port: 3306,
-  username: 'your_mysql_user',
-  password: 'Raradagugu123',
-  database: 'KAPO',
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   entities: [Product],
   synchronize: false, // use migrations in production
   migrations: ['src/migrations/*.ts'],
